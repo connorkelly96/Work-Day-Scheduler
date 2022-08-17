7307b46b92dc86118b3a1d126a8bb344611e60ab
connork96 <connorkelly96@yahoo.com>
2022-08-17 18:41:24 -0400

    added times to interact with local storage

--- assets/script.js
+++ assets/script.js
@@ -32,3 +32,15 @@ $(".time-block").each(function()   {
         $(this).addClass("future");
     }
 })};
+
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
+$("#hour-2 .description").val(localStorage.getItem("hour-2"));
+$("#hour-3 .description").val(localStorage.getItem("hour-3"));
+$("#hour-4 .description").val(localStorage.getItem("hour-4"));
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+trackTime();
