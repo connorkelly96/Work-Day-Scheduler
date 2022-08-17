731b19c37229b087f23302d1440bf041b9475b4f
connork96 <connorkelly96@yahoo.com>
2022-08-17 17:57:51 -0400

    linked save button to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
 var currentDay = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+//saveBtn
+$(document).ready(function() {
+$(".saveBtn").on("click", function () {
+    var textBlock = $(this).siblings(".description").val();
+    var timeBlock = $(this).parent().attr("id");
+}
+localStorage.setItem(timeBlock, textBlock);
+
+)});
