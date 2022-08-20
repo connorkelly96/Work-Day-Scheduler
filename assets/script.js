2fa1797f6b780bfced8e5366e3e9e30664ae5726
connork96 <connorkelly96@yahoo.com>
2022-08-20 19:43:25 -0400

    fixed my timeblock loop

--- assets/script.js
+++ assets/script.js
@@ -13,15 +13,15 @@ localStorage.setItem(timeBlock, textBlock);
 
 
 function trackTime() {
-    var currentTime = moment().hour();
+    var currentTime =  moment().hour();
 
 //timeblock loop
 $(".time-block").each(function()   {
-    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-
+    var timeBlock = parseInt($(this).attr("id").split("-")[1]);
+    console.log(timeBlock, currentTime)
     if (timeBlock < currentTime) {
-        $(this).addClass("future");
-        $(this).addClass("present");
+       // $(this).addClass("future");
+       // $(this).addClass("present");
         $(this).addClass("past");
     }
     else if (timeBlock == currentTime) {
@@ -41,11 +41,11 @@ $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
 $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-1 .description").val(localStorage.getItem("hour-1"));
-$("#hour-2 .description").val(localStorage.getItem("hour-2"));
-$("#hour-3 .description").val(localStorage.getItem("hour-3"));
-$("#hour-4 .description").val(localStorage.getItem("hour-4"));
-$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 trackTime();
 
  
